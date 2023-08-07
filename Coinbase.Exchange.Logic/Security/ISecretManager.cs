3100145692c87cd147ae72115baec7c278ebe3d7
valentine <Valentine.Azom@bmwithub.co.za>
2023-08-07 22:34:20 +0200

    added secret manager

---
+++ Coinbase.Exchange.Logic/Security/ISecretManager.cs
@@ -0,0 +1,15 @@
+﻿using Coinbase.Exchange.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coinbase.Exchange.Logic.Security
+{
+    public interface ISecretManager
+    {
+        string GetSignature(string body, string secret);
+        Task<Setting> GetSetting(string key);
+    }
+}
---
+++ Coinbase.Exchange.Logic/Security/SecretManager.cs
@@ -0,0 +1,25 @@
+﻿using Coinbase.Exchange.Domain.Entities;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Coinbase.Exchange.Logic.Security
+{
+    public class SecretManager : ISecretManager
+    {
+        public Task<Setting> GetSetting(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSignature(string body, string secret)
+        {
+            var bodybytes = Encoding.UTF8.GetBytes(body);
+            var secretbytes = Encoding.UTF8.GetBytes(secret);
+
+            var hash = new HMACSHA256(secretbytes);
+            var computedHash = hash.ComputeHash(bodybytes);
+
+            return Convert.ToHexString(computedHash);
+        }
+    }
+}
