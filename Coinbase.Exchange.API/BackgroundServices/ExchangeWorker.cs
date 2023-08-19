0e7aa3961ba453db6aff41e641700e76601fe237
valentine <Valentine.Azom@bmwithub.co.za>
2023-08-19 18:20:43 +0200

    completed the hub and background service

--- Coinbase.Exchange.API/BackgroundServices/ExchangeWorker.cs
+++ Coinbase.Exchange.API/BackgroundServices/ExchangeWorker.cs
@@ -1,6 +1,61 @@
-﻿namespace Coinbase.Exchange.API.BackgroundServices
+﻿using Coinbase.Exchange.API.Extensions;
+using Coinbase.Exchange.Logic.DataFeed;
+using Coinbase.Exchange.SharedKernel.Enums;
+using Coinbase.Exchange.SharedKernel.Models.Subscription;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace Coinbase.Exchange.API.BackgroundServices
 {
-    public class ExchangeWorker
+    public class ExchangeWorker : BackgroundService
     {
+        private readonly IMarketDataQueue _marketDataQueue;
+        private readonly IHubContext<ExchangeHub.ExchangeHub> _hubContext;
+        private readonly ILogger<ExchangeWorker> _logger;
+
+        public ExchangeWorker(IMarketDataQueue marketDataQueue, 
+            IHubContext<ExchangeHub.ExchangeHub> hubContext,
+            ILogger<ExchangeWorker> logger)
+        {
+            _marketDataQueue = marketDataQueue;
+            _hubContext = hubContext;
+            _logger = logger;
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            return base.StartAsync(cancellationToken);
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var marketData = await _marketDataQueue.DequeueAsync(stoppingToken);
+
+                try
+                {
+                    switch (marketData.Channel)
+                    {
+                        case WebSocketChannel.level2:
+                            var orderBook = JsonConvert.DeserializeObject<OrderBook>(marketData.Data);
+                            await orderBook!.Events.SendToClientsAsync(_hubContext);
+                            break;
+                        case WebSocketChannel.ticker:
+                            var tickerData = JsonConvert.DeserializeObject<Ticker>(marketData.Data);
+                            await tickerData!.Events.SelectMany(_ => _.Tickers).SendToClientsAsync(_hubContext);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
     }
 }
--- Coinbase.Exchange.API/ExchangeHub/ExchangeHub.cs
+++ Coinbase.Exchange.API/ExchangeHub/ExchangeHub.cs
@@ -1,15 +1,107 @@
 ﻿
+using Coinbase.Exchange.Domain.Entities;
+using Coinbase.Exchange.Domain.Specifications;
+using Coinbase.Exchange.Infrastructure.Repository;
 using Coinbase.Exchange.Logic.DataFeed;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Coinbase.Exchange.API.ExchangeHub
 {
+    [Authorize]
     public class ExchangeHub: Hub
     {
-        public ExchangeHub(ICoinbaseWebSocketClient coinbaseWebSocketClient
-            )
+        private readonly IRepository<Domain.Entities.Client> _clientRepository;
+        private readonly IRepository<Instrument> _instrumentRepository;
+        private readonly ICoinbaseWebSocketClient _webSocketClient;
+
+        public ExchangeHub(ICoinbaseWebSocketClient coinbaseWebSocketClient,
+            IRepository<Domain.Entities.Client> clientRepository,
+            IRepository<Instrument> instrumentRepository)
+        {
+            _clientRepository = clientRepository;
+            _instrumentRepository = instrumentRepository;
+            _webSocketClient = coinbaseWebSocketClient;
+        }
+
+        private string ClientId => Context.User!.Identity!.Name;
+
+        public override async Task OnConnectedAsync()
         {
             
+            var client = await _clientRepository.GetByIdAsync(ClientId);
+
+            if (client == null)
+                throw new ArgumentException($"Client with Id: {ClientId}");
+            foreach(var group in client.ProductGroups)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, group.Name);
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SubscribeAsync(string instrument)
+        {
+            
+            var group = await _instrumentRepository.GetBySpecAsync(new ClientsByProductGroupNameSpec(instrument));
+
+            if(group == null)
+            {
+                group = await _instrumentRepository.AddAsync(new Instrument
+                {
+                    Name = instrument
+                });
+
+                group.Clients.Add(new Domain.Entities.Client
+                {
+                    ClientId = ClientId
+                });
+                await _webSocketClient.SubScribe(new[] { instrument });
+                await _instrumentRepository.SaveChangesAsync();
+            }
+            else
+            {
+                var client = group.Clients.SingleOrDefault(_ => _.ClientId == ClientId);
+
+                if (client == null)
+                {
+
+                    group.Clients.Add(new Domain.Entities.Client
+                    {
+                        ClientId = ClientId
+                    });
+                    await _instrumentRepository.SaveChangesAsync();
+                }
+            }
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, instrument);
+        }
+
+        public async Task UnSubscribeAsync(string instrument)
+        {
+            var group = await _instrumentRepository.GetBySpecAsync(new ClientsByProductGroupNameSpec(instrument));
+
+            if(group == null)
+            {
+                throw new ArgumentNullException($"Client with Id: {ClientId} cannot unsubscribe from group that does not exist");
+            }
+
+            var client = await _clientRepository.GetByIdAsync(ClientId);
+
+            if(client == null)
+            {
+                throw new ArgumentNullException($"Client with Id: {ClientId} does not exist");
+            }
+
+            group.Clients.Remove(client);
+
+            if (!group.Clients.Any())
+            {
+                await _webSocketClient.UnSubscribe(new[] { group.Name });
+            }
+
+            await _instrumentRepository.SaveChangesAsync();
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, instrument);
         }
     }
 }
